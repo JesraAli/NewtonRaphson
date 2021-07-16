31c8e78da6c1626a3ed9e2bbe66869245fa77a65
Jorel Ali <68667988+Skylet100@users.noreply.github.com>
2021-07-16 15:09:14 +0100

    Added a tangent, an initial guess line and markings on the graph

--- main.js
+++ main.js
@@ -9,38 +9,93 @@ function squareroot(input_number, initial_guess, limit) {
 var Diagram = /** @class */ (function () {
     // Code to run when we initialize the Diagram
     function Diagram(number_root) {
+        this.MAX_X = 700;
+        this.MAX_Y = 700;
+        this.SCALE_X = 50;
+        this.SCALE_Y = 15;
         this.canvas = document.getElementById("myid");
         this.drawing_context = this.canvas.getContext("2d");
         this.number_root = number_root;
+        this.initial_guess = 5;
         this.update();
     }
     Diagram.prototype.update = function () {
         this.drawing_context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawing_context.setLineDash([1, 0]);
+        this.drawAxes();
+        // The line to draw
+        this.drawing_context.strokeStyle = "#0077cc";
+        this.drawing_context.lineWidth = 5;
+        this.drawing_context.beginPath();
+        for (var x = -this.MAX_X; x <= this.MAX_X; x += 0.2) {
+            var y = Math.pow(x, 2) - this.number_root;
+            this.drawing_context.lineTo(this.scaleX(x), this.scaleY(y));
+        }
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+        // Initial guess
+        this.drawing_context.strokeStyle = "#ff0000";
+        this.drawing_context.lineWidth = 2;
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(this.scaleX(this.initial_guess), this.MAX_Y / 2);
+        this.drawing_context.lineTo(this.scaleX(this.initial_guess), this.scaleY(Math.pow(this.initial_guess, 2) - this.number_root));
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+        this.drawTangent(this.initial_guess);
+    };
+    Diagram.prototype.drawTangent = function (x) {
+        this.drawing_context.setLineDash([2, 3]);
+        this.drawing_context.strokeStyle = "#000000";
+        // y - y1 = m(x - x1)
+        // y = m(x - x1) + y1
+        var m = 2 * x;
+        var y1 = Math.pow(x, 2) - this.number_root;
+        var yLeft = m * (-this.MAX_X - x) + y1;
+        var yRight = m * (this.MAX_X - x) + y1;
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(this.scaleX(-this.MAX_X), this.scaleY(yLeft));
+        this.drawing_context.lineTo(this.scaleX(this.MAX_X), this.scaleY(yRight));
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+    };
+    Diagram.prototype.drawAxes = function () {
         // The line to draw
         this.drawing_context.strokeStyle = "#000000";
         this.drawing_context.lineWidth = 1;
         // X-axis
         this.drawing_context.beginPath();
-        this.drawing_context.lineTo(0, 250);
-        this.drawing_context.lineTo(500, 250);
+        this.drawing_context.lineTo(0, this.MAX_Y / 2);
+        this.drawing_context.lineTo(this.MAX_X, this.MAX_Y / 2);
         this.drawing_context.stroke();
         this.drawing_context.closePath();
         // Y-axis
         this.drawing_context.beginPath();
-        this.drawing_context.lineTo(250, 0);
-        this.drawing_context.lineTo(250, 500);
+        this.drawing_context.lineTo(this.MAX_X / 2, 0);
+        this.drawing_context.lineTo(this.MAX_X / 2, this.MAX_Y);
         this.drawing_context.stroke();
         this.drawing_context.closePath();
-        // The line to draw
-        this.drawing_context.strokeStyle = "#0077cc";
-        this.drawing_context.lineWidth = 5;
-        this.drawing_context.beginPath();
-        for (var x = -500; x <= 500; x += 1) {
-            var y = Math.pow(x, 2) - this.number_root;
-            this.drawing_context.lineTo(x * 10 + 250, -y + 250);
+        // Ticks on the x-axis
+        for (var x = -this.MAX_X; x <= this.MAX_X; x += 1) {
+            this.drawing_context.beginPath();
+            this.drawing_context.lineTo(this.scaleX(x), this.MAX_Y / 2);
+            this.drawing_context.lineTo(this.scaleX(x), this.MAX_Y / 2 + 5);
+            this.drawing_context.stroke();
+            this.drawing_context.closePath();
         }
-        this.drawing_context.stroke();
-        this.drawing_context.closePath();
+        // Ticks on the y-axis
+        for (var y = -this.MAX_Y; y <= this.MAX_Y; y += 1) {
+            this.drawing_context.beginPath();
+            this.drawing_context.lineTo(this.MAX_Y / 2, this.scaleY(y));
+            this.drawing_context.lineTo(this.MAX_Y / 2 - 5, this.scaleY(y));
+            this.drawing_context.stroke();
+            this.drawing_context.closePath();
+        }
+    };
+    Diagram.prototype.scaleX = function (x) {
+        return x * this.SCALE_X + (this.MAX_X / 2);
+    };
+    Diagram.prototype.scaleY = function (y) {
+        return -(y * this.SCALE_Y) + (this.MAX_Y / 2);
     };
     return Diagram;
 }());
@@ -69,6 +124,7 @@ function update_viewer() {
     // Set iterations
     var iterations = document.getElementById("iterations");
     main_diagram.iterations = Number(iterations.value);
+    document.getElementById("iterations_number").innerText = iterations.value;
     // Do + generate
     document.getElementById("return_value").innerText = String(squareroot(main_diagram.number_root, main_diagram.initial_guess, main_diagram.iterations));
     main_diagram.update();
--- main.ts
+++ main.ts
@@ -9,6 +9,11 @@ function squareroot(input_number : number, initial_guess : number, limit : numbe
 // A class to house our diagram drawing logic
 class Diagram {
 
+    readonly MAX_X = 700;
+    readonly MAX_Y = 700;
+    readonly SCALE_X = 50;
+    readonly SCALE_Y = 15;
+
     private canvas : HTMLCanvasElement;                 // The canvas
     private drawing_context : CanvasRenderingContext2D; // The context (line width, color etc.)
 
@@ -22,42 +27,106 @@ class Diagram {
         this.drawing_context = this.canvas.getContext("2d");
 
         this.number_root = number_root;
+        this.initial_guess = 5;
         this.update();
     }
 
     public update() {
         this.drawing_context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // The line to draw
-        this.drawing_context.strokeStyle = "#000000";
-        this.drawing_context.lineWidth = 1;
-
-        // X-axis
-        this.drawing_context.beginPath();
-        this.drawing_context.lineTo(0, 250);
-        this.drawing_context.lineTo(500, 250);
-        this.drawing_context.stroke();
-        this.drawing_context.closePath();
-
-        // Y-axis
-        this.drawing_context.beginPath();
-        this.drawing_context.lineTo(250, 0);
-        this.drawing_context.lineTo(250, 500);
-        this.drawing_context.stroke();
-        this.drawing_context.closePath();
+        
+        this.drawing_context.setLineDash([1,0]);
+        this.drawAxes();
 
         // The line to draw
         this.drawing_context.strokeStyle = "#0077cc";
         this.drawing_context.lineWidth = 5;
 
         this.drawing_context.beginPath();
-        for (let x : number = -500; x <= 500; x+=1){
+        for (let x : number = -this.MAX_X; x <= this.MAX_X; x+=0.2){
             let y : number = x**2 - this.number_root;
-            this.drawing_context.lineTo(x * 10 + 250, -y + 250);
+            this.drawing_context.lineTo(this.scaleX(x), this.scaleY(y));
         }
         this.drawing_context.stroke();
         this.drawing_context.closePath();
+    
+        // Initial guess
+        this.drawing_context.strokeStyle = "#ff0000";
+        this.drawing_context.lineWidth = 2;
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(this.scaleX(this.initial_guess), this.MAX_Y / 2);
+        this.drawing_context.lineTo(this.scaleX(this.initial_guess), this.scaleY(this.initial_guess**2 - this.number_root));
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+
+        this.drawTangent(this.initial_guess);
     }
+
+    private drawTangent(x : number) : void {
+
+        this.drawing_context.setLineDash([2,3]);
+        this.drawing_context.strokeStyle = "#000000";
+        // y - y1 = m(x - x1)
+        // y = m(x - x1) + y1
+
+        let m = 2 * x;
+        let y1 = x**2 - this.number_root;
+
+        let yLeft = m * (-this.MAX_X - x) + y1;
+        let yRight = m * (this.MAX_X - x) + y1;
+
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(this.scaleX(-this.MAX_X), this.scaleY(yLeft));
+        this.drawing_context.lineTo(this.scaleX(this.MAX_X), this.scaleY(yRight));
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+    }
+
+    private drawAxes() : void {
+        // The line to draw
+        this.drawing_context.strokeStyle = "#000000";
+        this.drawing_context.lineWidth = 1;
+
+        // X-axis
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(0, this.MAX_Y / 2);
+        this.drawing_context.lineTo(this.MAX_X, this.MAX_Y / 2);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+
+        // Y-axis
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(this.MAX_X / 2, 0);
+        this.drawing_context.lineTo(this.MAX_X / 2, this.MAX_Y);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+
+        // Ticks on the x-axis
+        for (let x : number = -this.MAX_X; x <= this.MAX_X; x+=1){
+            this.drawing_context.beginPath();
+            this.drawing_context.lineTo(this.scaleX(x), this.MAX_Y / 2);
+            this.drawing_context.lineTo(this.scaleX(x), this.MAX_Y / 2 + 5);
+            this.drawing_context.stroke();
+            this.drawing_context.closePath();
+        }
+
+        // Ticks on the y-axis
+        for (let y : number = -this.MAX_Y; y <= this.MAX_Y; y+=1){
+            this.drawing_context.beginPath();
+            this.drawing_context.lineTo(this.MAX_Y / 2, this.scaleY(y));
+            this.drawing_context.lineTo(this.MAX_Y / 2 - 5, this.scaleY(y));
+            this.drawing_context.stroke();
+            this.drawing_context.closePath();
+        }
+    }
+
+    private scaleX(x : number) : number {
+        return x * this.SCALE_X + (this.MAX_X / 2);
+    }
+
+    private scaleY(y : number) : number {
+        return -(y * this.SCALE_Y) + (this.MAX_Y / 2);
+    }
+
 }
 
 let main_diagram = new Diagram(10);
@@ -87,9 +156,10 @@ function update_viewer() : void {
     // Set iterations
     let iterations : HTMLInputElement = document.getElementById("iterations") as HTMLInputElement;
     main_diagram.iterations = Number(iterations.value);
-
+    document.getElementById("iterations_number").innerText = iterations.value;
 
     // Do + generate
-    document.getElementById("return_value").innerText =String(squareroot(main_diagram.number_root, main_diagram.initial_guess, main_diagram.iterations));
+    document.getElementById("return_value").innerText = String(squareroot(main_diagram.number_root, main_diagram.initial_guess, main_diagram.iterations));
+    
     main_diagram.update();
 }
