b64f81cf833ccf4eec09b21545c51401c3bdd0f6
Jorel Ali <68667988+Skylet100@users.noreply.github.com>
2021-07-16 12:38:26 +0100

    Added a Visualiser

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-var message = "Hello world";
-console.log(message);
-function squareroot(thenumbertofindtherootof, guess, limit) {
-    var r = guess;
+function squareroot(input_number, initial_guess, limit) {
+    var r = initial_guess;
     for (var j = 1; j <= limit; j += 1) {
-        r = r - ((Math.pow(r, 2) - thenumbertofindtherootof) / (2 * r));
+        r = r - ((Math.pow(r, 2) - input_number) / (2 * r));
     }
-    console.log(r);
+    return r;
 }
-squareroot(10, 3, 800);
+console.log(squareroot(100, 3, 800));
+// A class to house our diagram drawing logic
+var Diagram = /** @class */ (function () {
+    // Code to run when we initialize the Diagram
+    function Diagram(number_root) {
+        this.canvas = document.getElementById("myid");
+        this.drawing_context = this.canvas.getContext("2d");
+        this.number_root = number_root;
+        this.update();
+    }
+    Diagram.prototype.update = function () {
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(0, 250);
+        this.drawing_context.lineTo(500, 250);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(250, 0);
+        this.drawing_context.lineTo(250, 500);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+        this.drawing_context.strokeStyle = "#0077cc";
+        this.drawing_context.lineWidth = 5;
+        this.drawing_context.beginPath();
+        for (var x = -500; x <= 500; x += 1) {
+            var y = Math.pow(x, 2) - this.number_root;
+            this.drawing_context.lineTo(x * 10 + 250, -y + 250);
+        }
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+    };
+    return Diagram;
+}());
+new Diagram(10);
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -6,4 +6,49 @@ function squareroot(input_number : number, initial_guess : number, limit : numbe
     return r;
 }
 
-console.log(squareroot(10, 3, 800));
+console.log(squareroot(100, 3, 800));
+
+// A class to house our diagram drawing logic
+class Diagram {
+
+    private canvas : HTMLCanvasElement;                 // The canvas
+    private drawing_context : CanvasRenderingContext2D; // The context (line width, color etc.)
+
+    private number_root : number;
+
+    // Code to run when we initialize the Diagram
+    constructor(number_root : number) {
+        this.canvas = document.getElementById("myid") as HTMLCanvasElement;
+        this.drawing_context = this.canvas.getContext("2d");
+
+        this.number_root = number_root;
+        this.update();
+    }
+
+    private update() {
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(0, 250);
+        this.drawing_context.lineTo(500, 250);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+
+        this.drawing_context.beginPath();
+        this.drawing_context.lineTo(250, 0);
+        this.drawing_context.lineTo(250, 500);
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+
+        this.drawing_context.strokeStyle = "#0077cc";
+        this.drawing_context.lineWidth = 5;
+
+        this.drawing_context.beginPath();
+        for (let x : number = -500; x <= 500; x+=1){
+            let y : number = x**2 - this.number_root;
+            this.drawing_context.lineTo(x * 10 + 250, -y + 250);
+        }
+        this.drawing_context.stroke();
+        this.drawing_context.closePath();
+    }
+}
+
+new Diagram(10);
